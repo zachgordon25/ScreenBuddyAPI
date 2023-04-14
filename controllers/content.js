4a2b1441796c95536eec98102a14b0d7e7cfd66a
Zach Gordon <zachgordon25@gmail.com>
2023-04-14 09:11:49 -0400

    get all content is working

---
+++ controllers/content.js
@@ -0,0 +1,22 @@
+const Pool = require('pg').Pool;
+
+const pool = new Pool({
+  user: process.env.DB_USER,
+  host: process.env.DB_HOST,
+  database: process.env.DB_NAME,
+  password: process.env.DB_PASSWORD,
+  port: process.env.DB_PORT,
+});
+
+const getContents = (request, response) => {
+  pool.query('SELECT * FROM content', (error, results) => {
+    if (error) {
+      throw error;
+    }
+    response.status(200).json(results.rows);
+  });
+};
+
+module.exports = {
+  getContents,
+};
