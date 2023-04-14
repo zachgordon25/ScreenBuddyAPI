fe3d2d810a62c348158958d253cea3b0e7ff09ae
Zach Gordon <zachgordon25@gmail.com>
2023-04-14 16:46:05 -0400

    content crud

--- index.js
+++ index.js
@@ -5,16 +5,16 @@ require('dotenv').config();
 const port = process.env.PORT || 3000;
 
 app.use(express.json());
-
-app.use(bodyParser.json());
-app.use(
-  bodyParser.urlencoded({
-    extended: true,
-  })
-);
+app.use(bodyParser.urlencoded({ extended: true }));
 
 const contentController = require('./controllers/content.js');
+
 app.get('/content', contentController.getContents);
+app.get('/content/:id', contentController.getContentById);
+app.get('/content/type/:type', contentController.getContentByType);
+app.post('/content', contentController.createContent);
+app.put('/content/:id', contentController.updateContent);
+app.delete('/content/:id', contentController.deleteContent);
 
 app.get('/test', (req, res) => {
   res.send(req.body);
