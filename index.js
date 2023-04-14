a93a735bf38b90ca946ccc96f55df11163066fda
Zach Gordon <zachgordon25@gmail.com>
2023-04-13 20:42:45 -0400

    testing api connection route for frontend

--- index.js
+++ index.js
@@ -2,8 +2,11 @@ const express = require('express');
 const app = express();
 const port = 3000;
 
-app.get('/', (req, res) => res.send('Hello World!'));
+app.use(express.json());
 
+app.get('/test', (req, res) => {
+  res.send(req.body);
+});
 app.listen(port, () => console.log(`Example app listening on port ${port}!`));
 
 module.exports = app;
