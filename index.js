e7972a55bb93084cbc7de46a78e0ce65ecc2e360
Zach Gordon <zachgordon25@gmail.com>
2023-04-13 21:13:57 -0400

    updated dotenv

--- index.js
+++ index.js
@@ -1,7 +1,8 @@
 const express = require('express');
 const app = express();
-const port = 3000;
-
+// require dotenv
+require('dotenv').config();
+const port = process.env.PORT || 3000;
 app.use(express.json());
 
 app.get('/test', (req, res) => {
