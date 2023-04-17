207e8feaf7956f86039aacb09ba8b8835d8df710
Zach Gordon <zachgordon25@gmail.com>
2023-04-17 09:31:46 -0400

    get content by different json params

--- index.js
+++ index.js
@@ -12,11 +12,9 @@ app.use(cors());
 
 const userController = require('./controllers/users.js');
 const contentController = require('./controllers/content.js');
-const contentController2 = require('./controllers/content2.js');
 
 app.use('/', userController);
 app.use('/getContent', contentController);
-app.use('/getContent2', contentController2);
 
 app.get('/test', (req, res) => {
   res.send(req.body);
