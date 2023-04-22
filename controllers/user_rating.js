de2abb411964da576b5dad2a313e64029e697412
Zach Gordon <zachgordon25@gmail.com>
2023-04-22 19:51:28 -0400

    update rating is working

--- controllers/user_rating.js
+++ controllers/user_rating.js
@@ -4,7 +4,7 @@ const pool = require('./pool.js');
 
 app.post('/addUserRating', (req, res) => {
   try {
-    const { user_id, id, title, name, poster_path, vote_average, media_type } = req.body.results[4];
+    const { user_id, id, title, name, poster_path, vote_average, media_type } = req.body;
     const contentTitle = media_type === 'movie' ? title : name;
     pool.query(
       'INSERT INTO user_ratings (user_id, content_id, title, image_url, rating, content_type) VALUES ($1, $2, $3, $4, $5, $6)',
@@ -17,10 +17,36 @@ app.post('/addUserRating', (req, res) => {
         media_type,
       ]
     );
-    res.status(201).send('Success');
+    res.status(201).send({
+      success: true,
+      message: 'User rating added successfully',
+    });
   } catch (err) {
     console.error(err);
-    res.status(500).json({ error: 'An error occurred' });
+    res.status(500).json({
+      success: false,
+      message: 'Error: rating not added',
+    });
+  }
+});
+
+app.put('/updateUserRating', async (req, res) => {
+  try {
+    const { rating, user_id, content_id } = req.body;
+    pool.query(
+      'UPDATE user_ratings SET rating = $1, updated_at = NOW() WHERE user_id = $2 AND content_id = $3',
+      [rating, user_id, content_id]
+    );
+    res.status(201).send({
+      success: true,
+      message: 'User rating updated successfully',
+    });
+  } catch (err) {
+    console.error(err);
+    res.status(500).json({
+      success: false,
+      message: 'Error: rating not updated',
+    });
   }
 });
 
