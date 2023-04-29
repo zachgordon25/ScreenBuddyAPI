76ffa796fe55bbc1695578b7cf6444abd77b9c98
Zach Gordon <zachgordon25@gmail.com>
2023-04-29 15:01:43 -0400

    duplicates are updated instead

--- controllers/user_rating.js
+++ controllers/user_rating.js
@@ -10,31 +10,47 @@ app.post('/addUserRating', async (req, res) => {
     if (contentCheck.rows.length === 0) {
       await pool.query(
         'INSERT INTO content (id, title, image_url, rating, content_type) VALUES ($1, $2, $3, $4, $5)',
-        [
-          content_id,
-          title,
-          `https://image.tmdb.org/t/p/w600_and_h900_bestv2${image_url}`,
-          rating,
-          content_type,
-        ]
+        [content_id, title, image_url, rating, content_type]
       );
     }
 
-    await pool.query(
-      'INSERT INTO user_ratings (user_id, content_id, title, image_url, rating, content_type) VALUES ($1, $2, $3, $4, $5, $6)',
-      [
-        user_id,
-        content_id,
-        title,
-        `https://image.tmdb.org/t/p/w600_and_h900_bestv2${image_url}`,
-        user_rating,
-        content_type,
-      ]
+    const ratingCheck = await pool.query(
+      'SELECT * FROM user_ratings WHERE user_id = $1 AND content_id = $2',
+      [user_id, content_id]
     );
-    res.status(201).send({
-      success: true,
-      message: 'User rating added successfully',
-    });
+
+    if (ratingCheck.rows.length === 0) {
+      await pool.query(
+        'INSERT INTO user_ratings (user_id, content_id, title, image_url, rating, content_type) VALUES ($1, $2, $3, $4, $5, $6)',
+        [user_id, content_id, title, image_url, user_rating, content_type]
+      );
+
+      res.status(201).send({
+        success: true,
+        message: 'User rating added successfully',
+      });
+    } else {
+      try {
+        const { rating, user_id, content_id } = req.body;
+
+        await pool.query(
+          'UPDATE user_ratings SET rating = $1, updated_at = NOW() WHERE user_id = $2 AND content_id = $3',
+          [rating, user_id, content_id]
+        );
+
+        res.status(201).send({
+          success: true,
+          message: 'User rating updated successfully',
+        });
+      } catch (err) {
+        console.error(err);
+        res.status(500).json({
+          success: false,
+          message: 'Error: rating not updated',
+          error: err,
+        });
+      }
+    }
   } catch (err) {
     console.error(err);
     res.status(500).json({
